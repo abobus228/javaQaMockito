935aa0cddca617fc64cbb69ffeefd1f55ec706dc
abobus228 <sashasafronov767@gmail.com>
2025-02-22 17:00:54 +0300

    Create class DataMovie.java

---
+++ src/main/java/ru/netology/dataClasses/DataMovie.java
@@ -0,0 +1,14 @@
+package ru.netology.dataClasses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class DataMovie {
+    private int ID;  // Уникальный ID фильма на площадке
+    private String movieTitle;  // Название фильма
+    private String filmGenre;  // Жанр фильма
+    private String srcImg;  // Путь до файла обложки
+    private double moveRating; // Рейтинг фильма
+}
